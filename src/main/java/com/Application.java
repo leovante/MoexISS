9ea07f2bfaccdd25bb0a83b9c43accdbb9a5e41d
leovante <leovante@live.ru>
2021-04-24 19:21:33 +0300

    minor fix for remote deploy

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,11 +4,18 @@ import com.configuration.SimpleMongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(new Class<?>[]{Application.class, SimpleMongoConfig.class}, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
