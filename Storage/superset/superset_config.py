491112f807039dcebb480e2335503d89a20b8052
Dmitry <>
2024-08-02 19:42:10 +0300

    fix superset connection to redis

--- Storage/superset/superset_config.py
+++ Storage/superset/superset_config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY', '')
 CACHE_CONFIG = {
@@ -26,3 +27,19 @@ SECRET_KEY = 'thisISaSECRET_1234'
 
 CONTENT_SECURITY_POLICY_WARNING = False
 PREVENT_UNSAFE_DB_CONNECTIONS = False
+
+TIME_GRAIN_ADDONS: Dict[str, str] = {
+    'PT2S': '2 second',
+    'PT5S': '5 second',
+    'PT10S': '10 second',
+    'PT30S': '30 second'
+}
+
+TIME_GRAIN_ADDON_EXPRESSIONS: Dict[str, Dict[str, str]] = {
+    'druid': {
+        'PT2S': '''TIME_FLOOR({col}, 'PT2S')''',
+        'PT5S': '''TIME_FLOOR({col},  'PT5S')''',
+        'PT10S': '''TIME_FLOOR({col}, 'PT10S')''',
+        'PT30S': '''TIME_FLOOR({col}, 'PT30S')'''
+    }
+}
